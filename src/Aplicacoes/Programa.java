5d1ab402b8a20c57b23323c4f0dee5f89b568482
Artannyel <artannyel>
2019-01-22 20:04:47 -0300

    Mensagem de boa noite

--- src/Aplicacoes/Programa.java
+++ src/Aplicacoes/Programa.java
@@ -4,5 +4,7 @@ public class Programa {
     
     public static void main(String[] args) {
         System.out.println("Bom dia");
+        System.out.println("Boa tarde");
+        System.out.println("Mensagens boa noite");
     }
 }
